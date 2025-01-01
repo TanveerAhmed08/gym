20f8947f9ebdd74fe221807a40f29f7877d20a93
TanveerAhmed08 <tanveerahmed08.1999@gmail.com>
2025-01-01 23:53:25 +0500

    home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,37 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import React from "react";
 import "./navbar.css";
 import Logo from "../images/logo.png";
 import { links } from "../data";
 import { FaBars } from "react-icons/fa6";
+import { MdOutlineClose } from "react-icons/md";
+
+
+
 const Navbar = () => {
+
+  const [isNavshowing, setIsNavshowing] = useState(false);
+
   return (
     <nav>
       <div className="container nav-container">
         <Link to="/" className="logo">
           <img src={Logo} alt="Nav Logo" />
         </Link>
-        <ul className="nav-links">
+        <ul className={`nav-links ${isNavshowing ? 'show-nav' : 'hide-nav'}`}>
           {links.map(({ name, path }, index) => {
             return (
-              <li>
-                <NavLink to={path}>{name}</NavLink>
+              <li key={index}>
+                <NavLink to={path} className={({ isActive }) => isActive ? "active-nav" : ""}>{name}</NavLink>
               </li>
             );
           })}
         </ul>
-        <button className="nav-toggle btn">
-          <FaBars />
+        <button className="nav-toggle-btn" onClick={() => setIsNavshowing(!isNavshowing)}>
+          {
+            isNavshowing ? <MdOutlineClose /> : <FaBars />
+          }
         </button>
       </div>
     </nav>
